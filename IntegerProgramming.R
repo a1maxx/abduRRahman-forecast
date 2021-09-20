1f1809769ffa5baf084ae4849d48f6abb6c41ebb
a1maxx <yvzabdurrahman@gmail.com>
2021-09-19 21:56:22 -0400

    Tries something

--- IntegerProgramming.R
+++ IntegerProgramming.R
@@ -26,9 +26,108 @@ result <- MIPModel() %>%
   
   set_objective(sum_expr(weights[i, j] * x[i, j, k], i = 1:n, j = 1:n,k=1:n)) %>% 
   add_constraint( sum_expr(x[i, j, k], k = 1:n) <= 1, i = 1:n,j=1:n) %>% 
-  add_constraint(x[i,j,k]<=5,i=1:n,j=1:n,k=1:n) %>% 
+  add_constraint(x[i,j,k]<=5,i=1:n,j=1:n,k=1:n) %>% ß
   solve_model(with_ROI("glpk", verbose = TRUE))
 
 get_solution(result, x[i, j, k]) %>% filter(value>0,variable=="x",i==1)
 
+grepl('livd','alive')
 
+sort(c('a','g','b'))
+
+a <- function(a1, a2) {
+  temp <- c()
+  for (i in a1) {
+    for (j in a2) {
+      if (grepl(i,j)) {
+        temp <- append(temp, i)
+        a1 <- a1[-which(a1==i)]
+        break
+      }
+    }
+
+  }
+  if(length(temp)>0)
+    return(sort(temp))
+  else 
+    return(c())
+}
+
+
+
+a1 = c("arp", "arp", "bull") 
+a2 = c("lively", "alive", "harp", "sharp", "armstrong")
+a(a1,a2)
+rm(list=ls())
+lng <-5
+wd <- 3
+squaresInRect <- function(lng, wd) {
+  totalSquares <- lng*wd
+  maxit <- min(lng,wd)
+  vec <- c()
+  if(lng!=wd){
+    while(sqrt(totalSquares)>1){
+      item <- min(floor(sqrt(totalSquares)),min(lng,wd))
+      vec <- append(vec,item)
+      totalSquares <- totalSquares - item**2
+      lng <- ifelse(lng-item==0,lng,lng-item)
+      wd <- ifelse(wd-item==0,wd,wd-item)
+    }
+    
+    return(append(vec,rep(1,totalSquares)))
+  }else{
+    return(NULL)
+  }
+}
+sqrt(280)
+sentence <- "is2 Thi1s T4est 3a"
+
+fsplit <-  strsplit(sentence," ")
+
+unlist(strsplit(fsplit[[1]][1],""))
+as.numeric(unlist(strsplit(fsplit[[1]][1],""))[grep("[0-9]+",unlist(strsplit(fsplit[[1]][1],"")))])
+
+sapply(fsplit, function(x) length(strsplit(x,"")) )
+
+sentence <- ""
+is.null(sentence)
+length(unlist(strsplit(sentence,""))) ==0 
+
+a <- sapply(fsplit[[1]],function(x) as.numeric(unlist(strsplit(x,"")) [ grep("[0-9]+",unlist(strsplit(x,""))) ]) ) 
+paste(names(sort(a)),collapse = " ")
+
+lapply(unlist(fsplit),function(x) split(x,""))
+  
+
+grep("[0-9]+",unlist(strsplit("th1s",split="")))
+
+as.character(strsplit("this",split=""))
+
+totalSquares <- 14*20
+squaresInRect(5,3)
+squaresInRect(3,5)
+squaresInRect(5,5)
+squaresInRect(20,14)
+
+
+lng <-3
+wd <- 5
+totalSquares <- lng*wd
+maxit <- min(lng,wd)
+vec <- c()
+if(lng!=wd){
+  while(sqrt(totalSquares)>1){
+    item <- min(floor(sqrt(totalSquares)),min(lng,wd))
+    vec <- append(vec,item)
+    totalSquares <- totalSquares - item**2
+    lng <- ifelse(lng-item==0,lng,lng-item)
+    wd <- ifelse(wd-item==0,wd,wd-item)
+    
+    # maxit <- min(max(lng-item,wd-item),maxit)
+  }
+  final <- append(vec,rep(1,totalSquares))
+}else{
+  return(NULL)
+}
+final
+sum(final**2)     
